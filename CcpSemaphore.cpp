ac8837bc3f0d06c4e964dab04bafff3e155b8dc7
CCP ChargeBack <35330827+ccp-chargeback@users.noreply.github.com>
2026-07-01 14:07:38 +0000

    Refactor CcpSemaphore.cpp
    - Remove OS specific duplication of the whole class
    - Add ifdefs in member functions for OS specific implementation details instead

--- CcpSemaphore.cpp
+++ CcpSemaphore.cpp
@@ -46,7 +46,17 @@ namespace
 }
 #endif
 
+// OS specific includes:
 #ifdef _WIN32
+// Nothing specific
+#elif defined(__APPLE__)
+#include <mach/semaphore.h>
+#include <mach/mach.h>
+#include <errno.h>  // Refactoring note: Original implementation included this after destructor and before Wait()
+#else
+#include <errno.h>  // Refactoring note: Original implementation included this after destructor and before Wait()
+#endif
+
 
 // Fully qualified, preferred constructor.
 CcpSemaphore::CcpSemaphore( const char* semaphoreName, uint32_t initialCount, uint32_t maximumCount )
@@ -67,101 +77,14 @@ CcpSemaphore::CcpSemaphore( const char* semaphoreName, uint32_t initialCount, ui
 	EnsureTelemetryLockAnnounced();
 #endif
 
+	// OS specific implementation:
+#ifdef _WIN32
 	m_semaphore = ::CreateSemaphore( 0, initialCount, maximumCount, 0 );
-}
-
-// Preferred constructor, with default value overloads (see header file for details)
-CcpSemaphore::CcpSemaphore( const char* semaphoreName )
-	: CcpSemaphore( semaphoreName, 0, 1 )
-{
-}
-
-CcpSemaphore::CcpSemaphore()
-	: CcpSemaphore( "CcpSemaphore", 0, 1 )
-{
-}
-
-CcpSemaphore::CcpSemaphore( uint32_t initialCount, uint32_t maximumCount )
-	: CcpSemaphore( "CcpSemaphore", initialCount, maximumCount )
-{
-}
-
-CcpSemaphore::~CcpSemaphore()
-{
-	::CloseHandle( m_semaphore );
-	delete[] m_semaphoreName;
-
-#if CCP_TELEMETRY_ENABLED
-	NotifyTelemetryLockTerminated( m_tracyLockContext );
-	m_tracyLockContext = nullptr;
-#endif
-}
-
-bool CcpSemaphore::Wait()
-{
-#if CCP_TELEMETRY_ENABLED
-	EnsureTelemetryLockAnnounced();
-	const bool notifyTracy = NotifyTelemetryBeforeLock( m_tracyLockContext );
-#endif
-
-	const bool result = ::WaitForSingleObject( m_semaphore, INFINITE ) == 0;
-
-#if CCP_TELEMETRY_ENABLED
-	NotifyTelemetryAfterLock( notifyTracy && result, m_tracyLockContext );
-#endif
-	return result;
-}
-
-bool CcpSemaphore::TimedWait( uint32_t timeout )
-{
-#if CCP_TELEMETRY_ENABLED
-	EnsureTelemetryLockAnnounced();
-	const bool notifyTracy = NotifyTelemetryBeforeLock( m_tracyLockContext );
-#endif
-
-	const auto result = ::WaitForSingleObject( m_semaphore, timeout ) == 0;
-
-#if CCP_TELEMETRY_ENABLED
-	NotifyTelemetryAfterLock( notifyTracy && result, m_tracyLockContext );
-#endif
-	return result;
-}
-
-void CcpSemaphore::Signal()
-{
-	::ReleaseSemaphore( m_semaphore, 1, 0 );
-
-#if CCP_TELEMETRY_ENABLED
-	NotifyTelemetryAfterUnlock( m_tracyLockContext );
-#endif
-}
-
-
 #elif defined(__APPLE__)
-
-#include <mach/semaphore.h>
-#include <mach/mach.h>
-
-// Fully qualified, preferred constructor. Note maximumCount is ignored.
-CcpSemaphore::CcpSemaphore( const char* semaphoreName, uint32_t initialCount, uint32_t maximumCount )
-{
-	// Make sure to keep our own copy of the semaphoreName
-	if (semaphoreName != nullptr)
-	{
-		const size_t strLen = std::strlen( semaphoreName ) + 1;
-		m_semaphoreName = new char[strLen];
-		strcpy_s( const_cast<char*>(m_semaphoreName), strLen, semaphoreName );
-	}
-	else
-	{
-		m_semaphoreName = nullptr;
-	}
-
-#if CCP_TELEMETRY_ENABLED
-	EnsureTelemetryLockAnnounced();
-#endif
-
 	semaphore_create( current_task(), &m_semaphore, SYNC_POLICY_FIFO, initialCount );
+#else
+	sem_init( &m_semaphore, 0, initialCount );
+#endif
 }
 
 // Preferred constructor, with default value overloads (see header file for details)
@@ -182,7 +105,15 @@ CcpSemaphore::CcpSemaphore( uint32_t initialCount, uint32_t maximumCount )
 
 CcpSemaphore::~CcpSemaphore()
 {
-    semaphore_destroy( current_task(), m_semaphore );
+	// OS specific implementation:
+#ifdef _WIN32
+	::CloseHandle( m_semaphore );
+#elif defined(__APPLE__)
+	semaphore_destroy( current_task(), m_semaphore );
+#else
+	sem_destroy( &m_semaphore );
+#endif
+
 	delete[] m_semaphoreName;
 
 #if CCP_TELEMETRY_ENABLED
@@ -191,8 +122,6 @@ CcpSemaphore::~CcpSemaphore()
 #endif
 }
 
-#include <errno.h>
-
 bool CcpSemaphore::Wait()
 {
 #if CCP_TELEMETRY_ENABLED
@@ -200,7 +129,14 @@ bool CcpSemaphore::Wait()
 	const bool notifyTracy = NotifyTelemetryBeforeLock( m_tracyLockContext );
 #endif
 
+	// OS specific implementation:
+#ifdef _WIN32
+	const bool result = ::WaitForSingleObject( m_semaphore, INFINITE ) == 0;
+#elif defined(__APPLE__)
 	const bool result = semaphore_wait( m_semaphore ) == KERN_SUCCESS;
+#else
+	const bool result = sem_wait( &m_semaphore ) == 0;
+#endif
 
 #if CCP_TELEMETRY_ENABLED
 	NotifyTelemetryAfterLock( notifyTracy && result, m_tracyLockContext );
@@ -215,105 +151,20 @@ bool CcpSemaphore::TimedWait( uint32_t timeoutInMs )
 	const bool notifyTracy = NotifyTelemetryBeforeLock( m_tracyLockContext );
 #endif
 
+	// OS specific implementation:
+#ifdef _WIN32
+	const bool result = ::WaitForSingleObject( m_semaphore, timeoutInMs ) == 0;
+#elif defined(__APPLE__)
 	mach_timespec_t mts;
 	mts.tv_sec = timeoutInMs / 1000;
 	mts.tv_nsec = ( timeoutInMs % 1000 ) * 1000000;
-	const auto result = semaphore_timedwait( m_semaphore, mts ) == KERN_SUCCESS;
-
-#if CCP_TELEMETRY_ENABLED
-	NotifyTelemetryAfterLock( notifyTracy && result, m_tracyLockContext );
-#endif
-	return result;
-}
-
-void CcpSemaphore::Signal()
-{
-    semaphore_signal( m_semaphore );
-
-#if CCP_TELEMETRY_ENABLED
-	NotifyTelemetryAfterUnlock( m_tracyLockContext );
-#endif
-}
-
+	const bool result = semaphore_timedwait( m_semaphore, mts ) == KERN_SUCCESS;
 #else
-
-// Fully qualified, preferred constructor. Note maximumCount is ignored
-CcpSemaphore::CcpSemaphore( const char* semaphoreName, uint32_t initialCount, uint32_t maximumCount )
-{
-	// Make sure to keep our own copy of the semaphoreName
-	if (semaphoreName != nullptr)
-	{
-		const size_t strLen = std::strlen( semaphoreName ) + 1;
-		m_semaphoreName = new char[strLen];
-		strcpy_s( const_cast<char*>(m_semaphoreName), strLen, semaphoreName );
-	}
-	else
-	{
-		m_semaphoreName = nullptr;
-	}
-
-#if CCP_TELEMETRY_ENABLED
-	EnsureTelemetryLockAnnounced();
-#endif
-
-	sem_init( &m_semaphore, 0, initialCount );
-}
-
-// Preferred constructor, with default value overloads (see header file for details)
-CcpSemaphore::CcpSemaphore( const char* semaphoreName )
-	: CcpSemaphore( semaphoreName, 0, 1 )
-{
-}
-
-CcpSemaphore::CcpSemaphore()
-	: CcpSemaphore( "CcpSemaphore", 0, 1 )
-{
-}
-
-CcpSemaphore::CcpSemaphore( uint32_t initialCount, uint32_t maximumCount )
-	: CcpSemaphore( "CcpSemaphore", initialCount, maximumCount )
-{
-}
-
-CcpSemaphore::~CcpSemaphore()
-{
-	sem_destroy( &m_semaphore );
-	delete[] m_semaphoreName;
-
-#if CCP_TELEMETRY_ENABLED
-	NotifyTelemetryLockTerminated( m_tracyLockContext );
-	m_tracyLockContext = nullptr;
-#endif
-}
-
-#include <errno.h>
-
-bool CcpSemaphore::Wait()
-{
-#if CCP_TELEMETRY_ENABLED
-	EnsureTelemetryLockAnnounced();
-	const bool notifyTracy = NotifyTelemetryBeforeLock( m_tracyLockContext );
-#endif
-
-	const bool result = sem_wait( &m_semaphore ) == 0;
-
-#if CCP_TELEMETRY_ENABLED
-	NotifyTelemetryAfterLock( notifyTracy && result, m_tracyLockContext );
-#endif
-	return result;
-}
-
-bool CcpSemaphore::TimedWait( uint32_t timeoutInMs )
-{
-#if CCP_TELEMETRY_ENABLED
-	EnsureTelemetryLockAnnounced();
-	const bool notifyTracy = NotifyTelemetryBeforeLock( m_tracyLockContext );
-#endif
-
 	timespec ts;
 	ts.tv_sec = timeoutInMs / 1000;
 	ts.tv_nsec = (timeoutInMs % 1000) * 1000000;
-	const auto result = sem_timedwait( &m_semaphore, &ts ) == 0;
+	const bool result = sem_timedwait( &m_semaphore, &ts ) == 0;
+#endif
 
 #if CCP_TELEMETRY_ENABLED
 	NotifyTelemetryAfterLock( notifyTracy && result, m_tracyLockContext );
@@ -323,15 +174,19 @@ bool CcpSemaphore::TimedWait( uint32_t timeoutInMs )
 
 void CcpSemaphore::Signal()
 {
+	// OS specific implementation:
+#ifdef _WIN32
+	::ReleaseSemaphore( m_semaphore, 1, 0 );
+#elif defined(__APPLE__)
+	semaphore_signal( m_semaphore );
+#else
 	sem_post( &m_semaphore );
+#endif
 
 #if CCP_TELEMETRY_ENABLED
 	NotifyTelemetryAfterUnlock( m_tracyLockContext );
 #endif
 }
-
-#endif
-
 
 #if CCP_TELEMETRY_ENABLED
 void CcpSemaphore::EnsureTelemetryLockAnnounced()
