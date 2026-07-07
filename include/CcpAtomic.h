76ef9a19266394c65e0dba6083a902ccbee920ac
ccptoebeans <ccptoebeans@users.noreply.github.com>
2026-07-07 09:36:59 +0000

    Reduce CcpAtomic.h to minimum implementation & Upgrade carbonengine registry to latest version

--- include/CcpAtomic.h
+++ include/CcpAtomic.h
@@ -7,27 +7,7 @@
 #ifndef CcpAtomic_h
 #define CcpAtomic_h
 
-#ifdef _MSC_VER
-	#if _MSC_VER >= 1700
-		#define CCP_HAS_ATOMIC 1
-	#endif
-#endif
-
-#ifdef __GNUG__
-	#if __GNUC__ == 4 && __GNUC_MINOR__ >= 7
-		#define CCP_HAS_ATOMIC 1
-	#endif
-#endif
-
-#ifdef __clang__
-#define CCP_HAS_ATOMIC 1
-#endif
-
-#if CCP_HAS_ATOMIC
-
 #include <atomic>
 #define CcpAtomic std::atomic
-
-#endif
 
 #endif // CcpAtomic_h
