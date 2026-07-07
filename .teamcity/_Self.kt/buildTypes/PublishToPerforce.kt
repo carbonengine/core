b9557c21f26991712dec6969c0fc29afcba592b5
ccptoebeans <ccptoebeans@users.noreply.github.com>
2026-07-07 13:22:30 +0000

    Allow switching of MSVC toolchain from Publish To Perforce build step

--- .teamcity/_Self.kt/buildTypes/PublishToPerforce.kt
+++ .teamcity/_Self.kt/buildTypes/PublishToPerforce.kt
@@ -39,8 +39,10 @@ class Publish(perforce_publish_path: String) : BuildType({
         param("env.TC_PERFORCE_PATH_TO_PUBLISH_INTO", "%perforce_path_to_publish_into%")
         param("env.EXECUTABLE_FILENAMES_MATCH", "")
 
-        select("reverse.dep.*.env.VISUAL_STUDIO_PLATFORM_TOOLSET", "v141", label = "Visual Studio Platform Toolset", description = "Specify the toolset for the build. e.g. v141 or v143.", display = ParameterDisplay.PROMPT,
-                options = listOf("v141 (2017)" to "v141", "v143 (2022)" to "v143"))
+        select("reverse.dep.*.VS_DEV_BAT_SWITCHES", "-arch=x64", label="Switches for VSDEV_BAT script (for selecting an MSVC toolchain)",
+            options = listOf(
+                "v141" to "-arch=x64",
+                "v145" to "-arch=x64 -vcvars_ver=14.1")
         text("reverse.dep.*.carbon_ref", "", label = "Ref  Carbon Component", description = "REF for carbon component e.g. refs/heads/main or refs/tags/v1.0.0 or refs/heads/frontier", display = ParameterDisplay.PROMPT, allowEmpty = true)
     }
 
